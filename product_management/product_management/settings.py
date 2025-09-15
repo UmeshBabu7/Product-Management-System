37b621d90b3cf7816ad518d6f306db602325416f
Umesh <umeshtamang129@gmail.com>
2025-09-15 08:52:29 +0545

    Django REST Framework settings

--- product_management/product_management/settings.py
+++ product_management/product_management/settings.py
@@ -112,3 +112,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
